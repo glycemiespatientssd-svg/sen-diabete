8939504a90a0f2954012bfadc21dd7d28753aa61
glycemiespatientssd-svg <glycemiespatientssd@gmail.com>
2025-11-27 07:06:53 +0100

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,18 +1,8 @@
 // functions/_middleware.js
 export async function onRequest(context) {
-  const { request, next } = context;
-  const url = new URL(request.url);
+  const { next } = context;
   
-  // Pages autorisées sans authentification
-  const publicPages = ['/login.html', '/admin.html', '/assets/'];
-  const isPublicPage = publicPages.some(page => url.pathname.includes(page));
-  
-  // Vérifier l'authentification via localStorage (côté client)
-  // Pour les pages protégées
-  if (!isPublicPage && (url.pathname === '/' || url.pathname === '/index.html')) {
-    // Renvoyer la page index normalement, la vérification se fera côté client
-    return next();
-  }
-  
+  // Laisser passer toutes les requêtes - l'auth se fait côté client
+  // C'est plus fiable pour les SPA (Single Page Applications)
   return next();
 }
